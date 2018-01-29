e11868a956f096814fd472da59efff337f685daf
dmitrysbn <dmitry.sbn@gmail.com>
2018-01-29 14:45:00 -0500

    Add Exercise 4.1

---
+++ exercise4-1.rb
@@ -0,0 +1,8 @@
+puts "Greetings, Dave. Please enter a number."
+number = gets.chomp.to_f
+
+if number >= 100
+  puts "Dave. That is a big number."
+else
+  puts "Dave. I would like you to dream a little bigger."
+end
