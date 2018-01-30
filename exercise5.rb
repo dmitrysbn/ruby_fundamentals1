df9aa404fae01217aa318fc92538ccbeb134e421
dmitrysbn <dmitry.sbn@gmail.com>
2018-01-29 19:12:44 -0500

    Add nutrition requirements, puke sequence, etc.

--- exercise5.rb
+++ exercise5.rb
@@ -1,33 +1,71 @@
 distance_from_home = 0
-workout_status = "on"
 
+workout_status = "on"
 # workout_status depends on annoyance level (must be <4, see below) and energy_level (must be >0)
-annoyance_level = 0 # initial value
-energy_level = 10 # initial value
+
+annoyance_level = 0
+# values: {0,1,2,3,4}. Once annoyance_level is 4, the program gets annoyed and ends the conversation and workout.
+# annoyance_level is reset to 0 once a user inputs a valid answer
+
+energy_level = 10
+# min value: 0. If energy_level is 0, we're too tired to continue the workout and take an Uber home.
+
+clif_bar_recency = 0
+# values: {0,1}. Whether a Clif bar has been eaten recently. If it has, and the user decides to run, they will puke and end the workout.
+# clif_bar_recency is reset to 0 if we choose to walk, instead of run, after eating it
 
 while workout_status == "on"
   if energy_level == 0
-    puts "Whew, I don't think I can make it any further. I'm calling an Uber. See ya."
+    puts "Whew, I don't think I can make it any further. Let's call an Uber."
     workout_status = "off"
 
   elsif energy_level < 3
-    puts "Let's take some rest and walk for a bit."
-    puts "...10 minutes later..."
-    distance_from_home += 1
-    energy_level = 3
-
-  elsif energy_level >= 3
-    puts "Would you like to walk or run?"
+    puts "#{(annoyance_level > 0) ? "" : "Let's take some rest and sit for a bit. "}Do you want a Clif bar or a banana?"
     answer = gets.chomp.downcase
 
-    if answer == "run"
+    if answer == "banana"
+      energy_level = 3
+      annoyance_level = 0
+      puts "...10 minutes later..."
+
+    elsif answer == "clif bar"
+      energy_level = 6
+      annoyance_level = 0
+      clif_bar_recency = 1
+      puts "...10 minutes later..."
+
+    else
+      annoyance_level += 1
+      if annoyance_level == 1
+        puts "Sorry, I didn't hear you properly."
+      elsif annoyance_level == 2
+        puts "Umm, ok."
+      elsif annoyance_level == 3
+        puts "What are you even saying?"
+      elsif annoyance_level == 4
+        puts "Whatever man, I'm going home. See ya."
+        workout_status = "off"
+      end
+    end
+
+  elsif energy_level >= 3
+    puts "#{(annoyance_level > 0) ? "So would " : "Would "}you like to walk or run?"
+    answer = gets.chomp.downcase
+
+    if (answer == "run") && (clif_bar_recency == 0)
       distance_from_home += 5
       energy_level -= 3
       puts "Distance from home is #{distance_from_home}km."
 
+    elsif (answer == "run") && (clif_bar_recency == 1)
+      puts "You've puked out your Clif bar. Gross! Let's get you home."
+      workout_status = "off"
+
     elsif answer == "walk"
       distance_from_home += 1
       energy_level += 2
+      clif_bar_recency = 0
+      # clif_bar_recency is reset since we chose to walk
       puts "Distance from home is #{distance_from_home}km."
 
     elsif answer == "go home"
@@ -36,7 +74,7 @@ while workout_status == "on"
         puts "#{distance_from_home}km is a pretty good workout! See you at home."
 
       else
-        puts "Well, alright. I guess we can try again tomorrow."
+        puts "Well, alright. I guess we can try again later this week."
       end
 
   # Case of invalid input -- annoyance_level keeps rising with each invalid input.
